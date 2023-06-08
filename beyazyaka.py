f3fb1537f9a31388a4a52961fc25af9df7352bbd
Ozcan YILDIZHAN <ozcan.14785@gmail.com>
2023-06-08 22:36:43 +0300

    finish beyazyaka.py
    Gerekli olan zam fonkiyonu yazılmıştır. Kullanıma hazır hale gelmiştir.

--- beyazyaka.py
+++ beyazyaka.py
@@ -19,4 +19,14 @@ class BeyazYaka(Calisan):
     def set_maas(self, maas):
         self.__maas = self.zam_hakki()
         return self.__maas
-    
+
+    def zam_hakki(self):
+        if self.get_tecrube() < 24:
+            return self.__tesvik_primi + self.__maas
+        elif self.get_tecrube() < 48 and self.get_maas() < 15000:
+            return (self.get_maas() % self.get_tecrube())*5 + self.__tesvik_primi + self.__maas
+        elif self.get_tecrube() >= 48 and self.get_maas() < 25000:
+            return (self.get_maas() % self.get_tecrube())*4 + self.__tesvik_primi + self.__maas
+
+    def __str__(self):
+        return f"{super().__str__()}\nTeşvik Prim: {self.__tesvik_primi}"
