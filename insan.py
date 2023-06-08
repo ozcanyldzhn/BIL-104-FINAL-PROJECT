a2adbffe83c690784e2d4666bee85689d0fa46c6
Ozcan YILDIZHAN <ozcan.14785@gmail.com>
2023-06-08 22:28:08 +0300

    uptade insan.py
    insan.py doyasına gerekli get/set metotları eklenerek __Str__ fonksiyonu yazılmıştır. insan.py dosyası hazırdır.

--- insan.py
+++ insan.py
@@ -6,3 +6,43 @@ class Insan:
         self.__yas = yas
         self.__cinsiyet = cinsiyet
         self.__uyruk = uyruk
+
+
+    def get_tc_no(self):
+        return self.__tc_no
+
+    def set_tc_no(self, tc_no):
+        self.__tc_no = tc_no
+
+    def get_ad(self):
+        return self.__ad
+
+    def set_ad(self, ad):
+        self.__ad = ad
+
+    def get_soyad(self):
+        return self.__soyad
+
+    def set_soyad(self, soyad):
+        self.__soyad = soyad
+
+    def get_yas(self):
+        return self.__yas
+
+    def set_yas(self, yas):
+        self.__yas = yas
+
+    def get_cinsiyet(self):
+        return self.__cinsiyet
+
+    def set_cinsiyet(self, cinsiyet):
+        self.__cinsiyet = cinsiyet
+
+    def get_uyruk(self):
+        return self.__uyruk
+
+    def set_uyruk(self, uyruk):
+        self.__uyruk = uyruk
+
+    def __str__(self):
+        return f"Ad: {self.__ad}\nSoyad: {self.__soyad}\nTC No: {self.__tc_no}\nYaş: {self.__yas}\nCinsiyet: {self.__cinsiyet}\nUyruk: {self.__uyruk}"
